b8f791827fb8727c28907dd6f48f51c813a57cda
Daiki Ihara <sasurau4@gmail.com>
2021-07-28 23:55:00 +0900

    Implement step4

--- README.md
+++ README.md
@@ -1,1 +1,18 @@
 ## seact
+
+sasurau4's React -> Seact
+
+See https://pomb.us/build-your-own-react/
+
+### Requirement
+
+- python3
+- Nodejs
+
+### Getting Started
+
+```
+yarn
+yarn build
+yarn start
+```
--- src/seact.ts
+++ src/seact.ts
@@ -1,22 +1,34 @@
 const TEXT_ELEMENT = 'TEXT_ELEMENT' as const
-type TextElement = {
+type SFiberCommonProperties = {
+  dom?: Container | null
+  parent?: HTMLFiberElement
+  child?: SFiber
+  sibling?: SFiber
+  props: {
+    children: SFiber[]
+  }
+}
+
+type TextFiberElement = {
   type: typeof TEXT_ELEMENT
   props: {
     nodeValue: string
-    children: never[]
   }
-}
-type DomElement = {
+} & SFiberCommonProperties
+type HTMLFiberElement = {
   type: string
-  props: {
-    nodeValue?: string
-    children: SElement[]
-  }
+} & SFiberCommonProperties
+
+type SFiber = TextFiberElement | HTMLFiberElement
+
+type Container = HTMLElement | Text
+
+// same as UNREACHED macro in Rust
+function UNREACHED(): Error {
+  throw new Error('UNREACHED!')
 }
 
-type SElement = TextElement | DomElement
-
-function createTextElement(text: string): TextElement {
+function createTextElement(text: string): TextFiberElement {
   return {
     type: 'TEXT_ELEMENT',
     props: {
@@ -30,7 +42,7 @@ export function createElement(
   type: string,
   props?: Record<string, string> | null,
   ...children: any[]
-): SElement {
+): SFiber {
   return {
     type,
     props: {
@@ -42,24 +54,91 @@ export function createElement(
   }
 }
 
-function render(element: SElement, container: HTMLElement | Text) {
+function createDom(fiber: SFiber): HTMLElement | Text {
   const dom =
-    element.type === TEXT_ELEMENT
+    fiber.type === TEXT_ELEMENT
       ? document.createTextNode('')
-      : document.createElement(element.type)
+      : document.createElement(fiber.type)
 
   const isProperty = (key: string) => key !== 'children'
-  Object.keys(element.props)
+  Object.keys(fiber.props)
     .filter(isProperty)
     .forEach((name) => {
       // @ts-ignore TODO
-      dom[name] = element.props[name]
+      dom[name] = fiber.props[name]
     })
-  element.props.children.forEach((child) => {
-    render(child, dom)
-  })
+  return dom
+}
 
-  container.appendChild(dom)
+function render(element: SFiber, container: Container): void {
+  nextUnitOfWork = {
+    dom: container,
+    // @ts-ignore 型がハマらない
+    props: {
+      children: [element],
+    },
+  }
+}
+
+let nextUnitOfWork: SFiber | null | undefined = null
+
+function workLoop(deadline: any): void {
+  let shouldYield = false
+  while (nextUnitOfWork && !shouldYield) {
+    nextUnitOfWork = performUnitOfWork(nextUnitOfWork)
+    shouldYield = deadline.timeRemaining() < 1
+  }
+  // NOTE: https://github.com/microsoft/TypeScript/issues/40807
+  requestIdleCallback(workLoop)
+}
+
+requestIdleCallback(workLoop)
+
+function performUnitOfWork(fiber: SFiber): SFiber | undefined {
+  if (!fiber.dom) {
+    fiber.dom = createDom(fiber)
+  }
+  if (fiber.parent && fiber.parent.dom) {
+    fiber.parent.dom.appendChild(fiber.dom)
+  }
+
+  const elements = fiber.props.children
+  let prevSibling: SFiber | null = null
+
+  // NOTE: create a newFiber for each child
+  for (let index = 0; index < elements.length; index++) {
+    const element = elements[index]
+    const newFiber: SFiber = {
+      type: element.type,
+      props: element.props,
+      parent: fiber,
+      dom: null,
+    }
+    if (index === 0) {
+      fiber.child = newFiber
+    } else if (prevSibling) {
+      prevSibling.sibling = newFiber
+    } else {
+      UNREACHED()
+    }
+
+    prevSibling = newFiber
+  }
+
+  if (fiber.child) {
+    return fiber.child
+  }
+  let nextFiber: SFiber = fiber
+  while (nextFiber) {
+    if (nextFiber.sibling) {
+      return nextFiber.sibling
+    }
+    if (nextFiber.parent) {
+      nextFiber = nextFiber.parent
+    } else {
+      UNREACHED()
+    }
+  }
 }
 
 export const Seact = {
